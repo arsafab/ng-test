e3c5cf6297bad7ff5a23972de2f8a4ec65fdd7ed
Dzmitry Bely <dmitriibely@gmail.com>
2021-01-26 19:29:37 +0300

    create shared and core modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule
+  ],
+  providers: []
+})
+export class SharedModule { }
