682eaa10cd6b52721be0277339fc9fc6014eafa5
Dzmitry Bely <dmitriibely@gmail.com>
2021-01-31 13:21:50 +0300

    add constants folder in shared

---
+++ src/app/shared/constants/federal-states.constant.ts
@@ -0,0 +1,18 @@
+export const FederalStates = [
+  'Bayern',
+  'Niedersachsen',
+  'Baden-Württemberg',
+  'Nordrhein-Westfalen',
+  'Brandenburg',
+  'Mecklenburg-Vorpommern',
+  'Hessen Hessen',
+  'Sachsen-Anhalt',
+  'Rheinland-Pfalz',
+  'Sachsen',
+  'Thüringen',
+  'Schleswig-Holstein',
+  'Saarland',
+  'Berlin',
+  'Hamburg',
+  'Bremen'
+];
