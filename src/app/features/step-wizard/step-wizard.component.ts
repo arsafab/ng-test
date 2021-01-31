0185fb94530b92aead50e3751715a5e669e03079
Dzmitry Bely <dmitriibely@gmail.com>
2021-01-31 13:22:19 +0300

    integrate customer form in step wizard

--- src/app/features/step-wizard/step-wizard.component.ts
+++ src/app/features/step-wizard/step-wizard.component.ts
@@ -1,15 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-step-wizard',
   templateUrl: './step-wizard.component.html',
   styleUrls: ['./step-wizard.component.css']
 })
-export class StepWizardComponent implements OnInit {
-  constructor(private formBuilder: FormBuilder) {}
-
-  public ngOnInit(): void {
-
-  }
+export class StepWizardComponent {
+  public isFormFilled = false;
 }
