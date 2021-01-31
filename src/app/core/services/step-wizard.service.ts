328c686e8aff62fcb37fbb0abd5279be6b638e85
Dzmitry Bely <dmitriibely@gmail.com>
2021-01-31 14:27:43 +0300

    add initial data for order in wizard service

--- src/app/core/services/step-wizard.service.ts
+++ src/app/core/services/step-wizard.service.ts
@@ -1,19 +1,34 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { IOrderInfo, CustomerInfo } from 'src/app/shared/models';
+import { IOrderItem, CustomerInfo } from 'src/app/shared/models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StepWizardService {
-  private readonly customerInfo: BehaviorSubject<any> = new BehaviorSubject({});
-  private readonly orderInfo: BehaviorSubject<any> = new BehaviorSubject({});
+  private readonly customerInfo: BehaviorSubject<CustomerInfo> = new BehaviorSubject(null);
+  private readonly orderInfo: BehaviorSubject<IOrderItem[]> = new BehaviorSubject(null);
+
+  constructor() {
+    // TODO: temporary solution for building summary block layout
+    this.setCustomerInfo({} as CustomerInfo);
+    this.setOrderInfo([
+      {
+        articleId: '1',
+        count: 2,
+        price: 32.80,
+        date: new Date(),
+        description: '',
+        title: 'Test'
+      }
+    ]);
+  }
 
   public getCustomerInfo(): Observable<CustomerInfo> {
     return this.customerInfo.asObservable();
   }
 
-  public getOrderInfo(): Observable<IOrderInfo> {
+  public getOrderInfo(): Observable<IOrderItem[]> {
     return this.orderInfo.asObservable();
   }
 
@@ -21,7 +36,7 @@ export class StepWizardService {
     this.customerInfo.next(data);
   }
 
-  public setOrderInfo(data: IOrderInfo): void {
+  public setOrderInfo(data: IOrderItem[]): void {
     this.orderInfo.next(data);
   }
 }
