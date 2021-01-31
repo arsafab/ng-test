ce45c7efba9d6e031c5653e21fa19b07926267a9
Dzmitry Bely <dmitriibely@gmail.com>
2021-01-31 13:22:08 +0300

    add customer form layout

--- src/app/features/step-wizard/customer-form/customer-form.component.ts
+++ src/app/features/step-wizard/customer-form/customer-form.component.ts
@@ -1,8 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { FederalStates } from 'src/app/shared/constants';
 
 @Component({
   selector: 'app-customer-form',
   templateUrl: './customer-form.component.html',
   styleUrls: ['./customer-form.component.css']
 })
-export class CustomerFormComponent {}
+export class CustomerFormComponent implements OnInit, OnDestroy {
+  @Output() public readonly valid: EventEmitter<boolean> = new EventEmitter();
+  public form: FormGroup;
+  public federalStates = FederalStates;
+
+  private readonly subscriptions: Subscription = new Subscription();
+
+  constructor(private readonly formBuilder: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      isPrivateCustomer: [true],
+      isUseTermsChecked: [false, Validators.required],
+      isApprovalTermsChecked: [false, Validators.required],
+      isContractTermsChecked: [false, Validators.required],
+      email: ['', [ Validators.required, Validators.email ]],
+      repeatedEmail: ['', [ Validators.required, this.checkEmailValidator ]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      street: ['', Validators.required],
+      houseNumber: ['', Validators.required],
+      city: ['', Validators.required],
+      zip: ['', Validators.required],
+      federalState: ['', Validators.required]
+    });
+
+    this.subscriptions.add(
+      this.form.valueChanges.subscribe(changes => {
+        this.valid.emit(this.form.valid);
+      })
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private checkEmailValidator = (): { notSame: boolean } | null => {
+    const email = this.form?.get('email').value;
+    const confirmedEmail = this.form?.get('repeatedEmail')?.value;
+
+    return email === confirmedEmail ? null : { notSame: true };
+  }
+}
